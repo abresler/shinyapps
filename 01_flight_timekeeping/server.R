bdac9ce8ba7f28c9dae2a20fb6006893986ce2cd
hibernado <rstudio@ip-172-31-36-248.eu-west-1.compute.internal>
2014-06-30 14:45:52 +0000

    2 Routes app

--- 01_flight_timekeeping/server.R
+++ 01_flight_timekeeping/server.R
@@ -1,27 +1,54 @@
 library(shiny)
+library(ggplot2)
 
-library(maps)
-counties <- readRDS("data/counties.rds")
-source("helpers.R")
 
 # Define server logic required
 shinyServer(function(input, output) {
+  flights <- read.csv("data/timekeeping.transformed.csv")
+  flights$airline_dir_x <- c(with(flights, paste(airline_x,direction_x) ))
+  flights$loc_dir_x <- c(with(flights, paste(location_x,direction_x) ))
+  
+    output$airlinehisttitle <- renderText({ 
+      paste("",input$route)
+    })
+  
+#   output$map <- renderPlot({
+#     
+#     args <- switch(input$var,
+#                    "Percent White" = list(counties$white, "darkgreen", "% White"),
+#                    "Percent Black" = list(counties$black, "black", "% Black"),
+#                    "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+#                    "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+#     
+#     args$min <- input$range[1]
+#     args$max <- input$range[2]
+#     
+#     do.call(percent_map, args)
+#   })
+    output$airlinehist <- renderPlot({
+    p <-  ggplot(flights[flights$direction_x == input$route,], aes(flightduration, fill=airline_x)) + geom_density(alpha=0.3)
+     print(p)
+    })
+  
+   #     temp <- switch(input$route,
+#                        "Dublin to London Gatwick" = flights[flights$airline_dir_x == "Dublin departures",],
+#                        "London Gatwick to Dublin" = flights[flights$airline_dir_x == "Dublin arrivals",]
+#     )
+# 
+#    
+#    if (input$route == "Dublin to London Gatwick") {
+#      # "Dublin to London Gatwick"
+#      mpgData <- data.frame(flights[flights$airline_dir_x == "Dublin departures",])
+#    }
+#    else {
+#      # cyl and gear
+#      mpgData <- data.frame(mpg = mtcars$mpg, var = factor(mtcars[[input$variable]]))
+#    }
+#   cat (summary(mpgData)) 
+#  p <-  ggplot(mpgData, aes(flightduration, fill=airline_x) ) + geom_density(alpha=0.3)
+ # p <- ggplot(temp, aes(temp$flightduration, fill=temp$airline_x)) + geom_density(alpha=0.3)
+ # print(p)
 
-  output$airlinehisttitle <- renderText({ 
-    paste("",input$route)
-  })
-  
-  output$map <- renderPlot({
-    args <- switch(input$var,
-                   "Percent White" = list(counties$white, "darkgreen", "% White"),
-                   "Percent Black" = list(counties$black, "black", "% Black"),
-                   "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
-                   "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
-    
-    args$min <- input$range[1]
-    args$max <- input$range[2]
-    
-    do.call(percent_map, args)
-  })
-  
+
+
   })
--- 01_flight_timekeeping/ui.R
+++ 01_flight_timekeeping/ui.R
@@ -1,5 +1,18 @@
 library(shiny)
 
+googleAnalytics <- function(account="UA-52253018-1"){
+  HTML(paste("<script>
+               (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+               })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+             
+             ga('create', '",account,"', 'shinyapps.io');
+             ga('send', 'pageview');
+             
+             </script>"))
+}
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
@@ -11,29 +24,32 @@ shinyUI(fluidPage(
                            helpText("Choose a route") ),
                   fluidRow(
                            selectInput("route", label = "", 
-                                       #choices = list("Dublin to London Gatwick" = 1, 
-                                       #             "London Gatwick to Dublin" = 2) 
-                                       choices = c("Dublin to London Gatwick","London Gatwick to Dublin")
-                                       )), 
-                  
-                  helpText("Create demographic maps with 
-        information from the 2010 US Census."),
-                  
-                  selectInput("var", 
-                              label = "Choose a variable to display",
-                              choices = c("Percent White", "Percent Black",
-                                          "Percent Hispanic", "Percent Asian"),
-                              selected = "Percent White"),
-                  
-                  sliderInput("range", 
-                              label = "Range of interest:",
-                              min = 0, max = 100, value = c(0, 100))
+                                       choices = list("Dublin to London Gatwick" = "departures", 
+                                                    "London Gatwick to Dublin" = "arrivals") ,
+                                       #choices = c("Dublin to London Gatwick","London Gatwick to Dublin"),
+                                       selected = "departures"
+                                       ))
+#                   , 
+#                   
+#                   helpText("Create demographic maps with 
+#         information from the 2010 US Census."),
+#                   
+#                   selectInput("var", 
+#                               label = "Choose a variable to display",
+#                               choices = c("Percent White", "Percent Black",
+#                                           "Percent Hispanic", "Percent Asian"),
+#                               selected = "Percent White"),
+#                   
+#                   sliderInput("range", 
+#                               label = "Range of interest:",
+#                               min = 0, max = 100, value = c(0, 100))
 
     ),
     mainPanel(
               plotOutput("airlinehist"),
               h5(textOutput("airlinehisttitle"), align = "center"),
-              plotOutput("map")
+              googleAnalytics()
+           #   plotOutput("map")
     )
   )
 ))
